edc3dfd85569f89a7642f1419c4d222de85719a0
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-14 15:13:05 +0700

    chore: initial commit

---
+++ src/test/java/vn/io/datvutech/beatbuddy/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.io.datvutech.beatbuddy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
